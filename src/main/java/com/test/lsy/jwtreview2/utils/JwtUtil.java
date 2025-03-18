c73d32b6f3fbcc02ccc383126adb0174e22cadee
이상엽 <infightersy@naver.com>
2025-03-18 16:07:18 +0900

    토큰 만료일 수정

--- src/main/java/com/test/lsy/jwtreview2/utils/JwtUtil.java
+++ src/main/java/com/test/lsy/jwtreview2/utils/JwtUtil.java
@@ -13,12 +13,15 @@ public class JwtUtil {
 
     public static String createToken(PrincipalDetails principalDetails) {
 
-        Date expiration = new Date(System.currentTimeMillis() + 1000 * 60 * 60);
+//        Date expiration = new Date(System.currentTimeMillis() + 1000 * 60 * 60);
+        Date expiration = new Date(System.currentTimeMillis() + 1000 * 60 * 1);  // 1분 후
+
+
         log.info("Token expiration: {}", expiration);
 
         return JWT.create()
                 .withSubject(principalDetails.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .withExpiresAt(expiration)
                 .withClaim("id", principalDetails.getUser().getId()) // 사용자 ID
                 .withClaim("username", principalDetails.getUser().getUsername()) // 사용자 이름
                 .sign(Algorithm.HMAC512(JwtProperties.SECRET)); // 암호화 키
